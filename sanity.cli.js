ab7f7f7a51548e55fa5a7c7e310e28158fbc16b8
Rishav045 <82647816+Rishav045@users.noreply.github.com>
2022-12-31 17:43:06 +0530

    Summary

---
+++ sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '7ygdxgwk',
+    dataset: 'production'
+  }
+})
---
+++ schemas/index.js
@@ -0,0 +1,7 @@
+import user from './user.js'
+import pin from './pin.js'
+import save from './save.js'
+import comment from './comment.js'
+import postedBy from './postedBy.js'
+
+export const schemaTypes = [user,pin , save, comment , postedBy]
